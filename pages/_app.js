f98147c5c7995d260c3b05d7dcaa26f63578ae83
Natasza <natasza@inigra.eu>
2025-07-10 09:59:38 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Rodzic-Chat AI – zadaj anonimowe pytanie o swoje dziecko</title>
+        <meta
+          name="description"
+          content="Anonimowy czat oparty na AI. Zadaj pytanie dotyczące rozwoju dziecka i otrzymaj informację bazującą na literaturze."
+        />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
